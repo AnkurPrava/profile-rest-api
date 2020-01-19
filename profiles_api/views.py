e267d9194c156bccea54b74dc4a7245f46ce6d51
ankur prava <ankur.prava@afourtech.com>
2020-01-19 13:02:28 +0530

    Creating APIView and urls

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test rest_framework ApiView """
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP methods such as (get, post, update, patch, delete)',
+            'Is similar to traditional Django Views',
+            'Gives you most control over your Applications',
+            'Is mapped manually to the URLs'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    
