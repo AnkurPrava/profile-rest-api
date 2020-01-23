e8df5069df7995ef8f41fa8ba3041eb79a027e5c
ankur.prava <ankur.prava@afourtech.com>
2020-01-23 09:14:33 +0530

    Created user profiles with authentication and search filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import serializers
+from profiles_api import permissions
+from profiles_api import models
+
 
 
 class HelloApiView(APIView):
@@ -88,3 +94,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle the creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email', )
